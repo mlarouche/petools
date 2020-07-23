089653ed1d1a401369b9b22cb1db0b90a65fec44
Michaël Larouche <michael.larouche@gmail.com>
2020-07-23 11:17:31 -0400

    Read sections and start import directories

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
+const Allocator = std.mem.Allocator;
 
 // Latest PE format documentation can be found at https://docs.microsoft.com/en-us/windows/win32/debug/pe-format
 
 // MS-DOS program pre-assembled
 const msdos_program_sub = @embedFile("msdos_program_stub.bin");
 
-/// Characterics are flags used to indicate attributes of the object or image file.
-pub const Characterics = packed struct {
+/// ImageCharacteristics are flags used to indicate attributes of the object or image file.
+pub const ImageCharacteristics = packed struct {
     /// Image only, used to indicate if the image contains base relocations
     reloc_stripped: bool,
     /// Image only. This indicates that the image file is valid and can be run. If this flag is not set, it indicates a linker error.
@@ -156,7 +157,7 @@ pub const COFFHeader = packed struct {
     /// his value should be zero for an object file.
     sizeof_optional_header: u16,
     /// The flags that indicate the attributes of the file
-    characteristics: Characterics,
+    characteristics: ImageCharacteristics,
 };
 
 pub const OptionalHeaderMagic = packed enum(u16) {
@@ -174,6 +175,31 @@ pub const DataDirectory = packed struct {
 };
 
 const ImageNumberOfDirectoryEntries = 16;
+
+pub const DLLCharacteristics = struct {
+    /// Image can handle a high entropy 64-bit virtual address space.
+    pub const HighEntroyVA = 0x0020;
+    /// DLL can be relocated at load time.
+    pub const DynamicBase = 0x0040;
+    /// Code Integrity checks are enforced.
+    pub const ForceIntegrity = 0x0080;
+    /// Image is NX compatible.
+    pub const NXCompat = 0x0100;
+    /// Isolation aware, but do not isolate the image.
+    pub const NoIsolation = 0x0200;
+    /// Does not use structured exception (SE) handling. No SE handler may be called in this image.
+    pub const NoSEH = 0x0400;
+    /// Do not bind the image.
+    pub const NoBind = 0x0800;
+    /// Image must execute in an AppContainer.
+    pub const AppContainer = 0x1000;
+    /// A WDM driver.
+    pub const WDMDriver = 0x2000;
+    /// Image supports Control Flow Guard.
+    pub const GuardCF = 0x4000;
+    /// Terminal Server aware.
+    pub const TerminalServerAware = 0x8000;
+};
 
 pub const OptionalHeader32 = packed struct {
     magic: OptionalHeaderMagic,
@@ -242,11 +268,198 @@ pub const OptionalHeader64 = packed struct {
     data_directory: [ImageNumberOfDirectoryEntries]DataDirectory,
 };
 
+const ImageSizeOfShortName = 8;
+
+pub const SectionCharacteristics = struct {
+    /// The section should not be padded to the next boundary.
+    /// This flag is obsolete and is replaced by IMAGE_SCN_ALIGN_1BYTES. This is valid only for object files.
+    pub const NoPad = 0x00000008;
+
+    /// The section contains executable code.
+    pub const ContentCode = 0x00000020;
+
+    /// The section contains initialized data
+    pub const InitializedData = 0x00000040;
+
+    /// The section contains uninitialized data
+    pub const UninitializedData = 0x00000080;
+
+    /// Reserved for future use
+    pub const LinkOther = 0x00000100;
+
+    /// The section contains comments or other information.
+    /// The .drectve section has this type. This is valid for object files only.
+    pub const LinkInfo = 0x00000200;
+
+    /// The section will not become part of the image. This is valid only for object files.
+    pub const LinkRemove = 0x00000800;
+
+    /// The section contains COMDAT data. This is valid only for object files.
+    pub const LinkComdat = 0x00001000;
+
+    /// The section contains data referenced through the global pointer (GP).
+    pub const GpRel = 0x00008000;
+
+    /// Reserved for future use.
+    pub const MemPurgeable = 0x00020000;
+
+    /// Reserved for future use.
+    pub const MemLocked = 0x00040000;
+
+    /// Reseved for future use.
+    pub const MemPreload = 0x00080000;
+
+    /// Align data on a 1-byte boundary. Valid only for object files.
+    pub const Align1Bytes = 0x00100000;
+
+    /// Align data on a 2-byte boundary. Valid only for object files.
+    pub const Align2Bytes = 0x00200000;
+
+    /// Align data on a 4-byte boundary. Valid only for object files.
+    pub const Align4Bytes = 0x00300000;
+
+    /// Align data on an 8-byte boundary. Valid only for object files.
+    pub const Align8Bytes = 0x00400000;
+
+    /// Align data on a 16-byte boundary. Valid only for object files.
+    pub const Align16Bytes = 0x00500000;
+
+    /// Align data on a 32-byte boundary. Valid only for object files.
+    pub const Align32Bytes = 0x00600000;
+
+    /// Align data on a 64-byte boundary. Valid only for object files.
+    pub const Align64Bytes = 0x00700000;
+
+    /// Align data on a 128-byte boundary. Valid only for object files.
+    pub const Align128Bytes = 0x00800000;
+
+    /// Align data on a 256-byte boundary. Valid only for object files.
+    pub const Align256Bytes = 0x00900000;
+
+    /// Align data on a 512-byte boundary. Valid only for object files.
+    pub const Align512Bytes = 0x00A00000;
+
+    /// Align data on a 1024-byte boundary. Valid only for object files.
+    pub const Align1024Bytes = 0x00B00000;
+
+    /// Align data on a 2048-byte boundary. Valid only for object files.
+    pub const Align2048Bytes = 0x00C00000;
+
+    /// Align data on a 4096-byte boundary. Valid only for object files.
+    pub const Align4096Bytes = 0x00D00000;
+
+    /// Align data on an 8192-byte boundary. Valid only for object files.
+    pub const Align8192Bytes = 0x00E00000;
+
+    /// The section contains extended relocations.
+    pub const LinkNumRelocOverflow = 0x01000000;
+
+    /// The section can be discarded as needed.
+    pub const MemDiscardable = 0x02000000;
+
+    /// The section cannot be cached.
+    pub const MemNotCached = 0x04000000;
+
+    /// The section is not pageable.
+    pub const MemNotPaged = 0x08000000;
+
+    /// The section can be shared in memory
+    pub const MemShared = 0x10000000;
+
+    /// The section can be executed as code.
+    pub const MemExecute = 0x20000000;
+
+    /// The section can be read.
+    pub const MemRead = 0x40000000;
+
+    /// The section can be written to.
+    pub const MemWrite = 0x80000000;
+};
+
+pub const SectionHeader = packed struct {
+    name: [ImageSizeOfShortName]u8,
+    misc: packed union {
+        physical_address: u32,
+        virtual_size: u32,
+    },
+    virtual_address: u32,
+    sizeof_raw_data: u32,
+    pointer_to_raw_data: u32,
+    pointer_to_relocations: u32,
+    pointer_to_line_numbers: u32,
+    number_of_relocations: u16,
+    number_of_line_numbers: u16,
+    characteristics: u32,
+};
+
+pub const ImportDirectoryEntry = packed struct {
+    lookup_relative_address: u32,
+    timestamp: u32,
+    forwarder_chain: u32,
+    name_relative_address: u32,
+    import_table_relative_address: u32,
+
+    const Self = @This();
+
+    pub fn isEndEntry(self: Self) bool {
+        if (self.lookup_relative_address == 0 and self.timestamp == 0 and self.forwarder_chain == 0 and self.name_relative_address == 0 and self.import_table_relative_address == 0) {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+pub const ImportLookupEntry32 = packed struct {
+    value: union {
+        ordinal: u16,
+        name_relative_address: u31,
+    },
+    import_by_ordinal: bool,
+};
+
+pub const ImportLookupEntry64 = packed struct {
+    value: union {
+        ordinal: u16,
+        name_relative_address: u62,
+    },
+    import_by_ordinal: bool,
+};
+
+pub const ImportHintEntry = packed struct {
+    hint: u16,
+    name: []const u8,
+};
+
+pub const ImportLookupEntry = union(enum) {
+    pe32: ImportLookupEntry32,
+    pe64: ImportLookupEntry64,
+};
+
+pub const ImageDirectoryEntryIndex = struct {
+    pub const Export = 0;
+    pub const Import = 1;
+    pub const Resource = 2;
+    pub const Exception = 3;
+    pub const Security = 4;
+    pub const BaseReloc = 5;
+    pub const Debug = 6;
+    pub const Architecture = 7;
+    pub const GlobalPtr = 8;
+    pub const ThreadLocalPointer = 9;
+    pub const LoadConfig = 10;
+    pub const BoundImport = 11;
+    pub const ImportAddressTable = 12;
+    pub const DelayImport = 13;
+    pub const ComDescriptor = 14;
+};
+
 comptime {
     std.debug.assert(@sizeOf(DOSHeader) == 64);
     std.debug.assert(@sizeOf(COFFHeader) == 20);
     std.debug.assert(@sizeOf(OptionalHeader32) == 224);
     std.debug.assert(@sizeOf(OptionalHeader64) == 240);
+    std.debug.assert(@sizeOf(SectionHeader) == 40);
 }
 
 const PEMagic = [4]u8{ 'P', 'E', 0, 0 };
@@ -265,6 +478,21 @@ fn peFile(comptime PEHeaderType: type) type {
     return struct {
         dos_header: DOSHeader,
         pe_header: PEHeaderType,
+        section_headers: []SectionHeader,
+
+        const Self = @This();
+
+        pub fn getSection(self: Self, name: []const u8) !SectionHeader {
+            for (self.section_headers) |section| {
+                const min_len = std.math.min(section.name.len, name.len);
+
+                if (std.mem.order(u8, section.name[0..min_len], name) == .eq) {
+                    return section;
+                }
+            }
+
+            return error.SectionNotFound;
+        }
     };
 }
 
@@ -276,9 +504,25 @@ pub const PEFile = union(enum) {
     pe64: PEFile64,
 };
 
+fn readSections(allocator: *Allocator, in_stream: var, number_sections: usize) ![]SectionHeader {
+    var section_headers = try allocator.alloc(SectionHeader, number_sections);
+    errdefer allocator.free(section_headers);
+
+    var index: usize = 0;
+    while (index < number_sections) : (index += 1) {
+        section_headers[index] = try in_stream.readStruct(SectionHeader);
+    }
+
+    return section_headers;
+}
+
 pub fn main() anyerror!void {
-    const args = try std.process.argsAlloc(std.heap.page_allocator);
-    defer std.process.argsFree(std.heap.page_allocator, args);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const arena_allocator = &arena.allocator;
+
+    const args = try std.process.argsAlloc(arena_allocator);
+    defer std.process.argsFree(arena_allocator, args);
 
     if (args.len < 1) {
         std.debug.warn("No input file provided.\n", .{});
@@ -287,7 +531,7 @@ pub fn main() anyerror!void {
 
     const cwd = std.fs.cwd();
 
-    const full_path = try std.fs.path.resolve(std.heap.page_allocator, &[_][]u8{args[1]});
+    const full_path = try std.fs.path.resolve(arena_allocator, &[_][]u8{args[1]});
 
     var file = try cwd.openFile(full_path, .{});
 
@@ -328,7 +572,21 @@ pub fn main() anyerror!void {
         pe_file.pe_header.coff = coff_header;
         pe_file.pe_header.optional = try in_stream.readStruct(OptionalHeader32);
 
-        std.debug.warn("\nOptional: {}\n", .{pe_file.pe_header.optional});
+        std.debug.warn("\nOptional:\n\n{}\n", .{pe_file.pe_header.optional});
+
+        std.debug.warn("\nRVA:\n\n", .{});
+
+        for (pe_file.pe_header.optional.data_directory) |rva| {
+            std.debug.warn("{}\n", .{rva});
+        }
+
+        pe_file.section_headers = try readSections(arena_allocator, in_stream, pe_file.pe_header.coff.number_of_sections);
+
+        std.debug.warn("\nSections:\n\n", .{});
+
+        for (pe_file.section_headers) |section| {
+            std.debug.warn("{}\n", .{section});
+        }
     } else if (optional_header_size == @sizeOf(OptionalHeader64)) {
         std.debug.warn("PE/COFF x86-64 executable\n", .{});
 
@@ -339,6 +597,33 @@ pub fn main() anyerror!void {
         pe_file.pe_header.coff = coff_header;
         pe_file.pe_header.optional = try in_stream.readStruct(OptionalHeader64);
 
-        std.debug.warn("\nOptional: {}\n", .{pe_file.pe_header.optional});
+        std.debug.warn("\nOptional:\n\n{}\n", .{pe_file.pe_header.optional});
+
+        std.debug.warn("\nRVA:\n\n", .{});
+
+        for (pe_file.pe_header.optional.data_directory) |rva| {
+            std.debug.warn("{}\n", .{rva});
+        }
+
+        pe_file.section_headers = try readSections(arena_allocator, in_stream, pe_file.pe_header.coff.number_of_sections);
+
+        std.debug.warn("\nSections:\n\n", .{});
+
+        for (pe_file.section_headers) |section| {
+            std.debug.warn("{}\n", .{section});
+        }
+
+        const import_section = try pe_file.getSection(".idata");
+
+        try seek_stream.seekTo(import_section.pointer_to_raw_data);
+
+        var image_directory_entry: ImportDirectoryEntry = try in_stream.readStruct(ImportDirectoryEntry);
+
+        std.debug.warn("ImportDirectory Entries:\n\n", .{});
+
+        while (!image_directory_entry.isEndEntry()) {
+            std.debug.warn("{}\n", .{image_directory_entry});
+            image_directory_entry = try in_stream.readStruct(ImportDirectoryEntry);
+        }
     }
 }
